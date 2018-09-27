67d3589f4698649a22633f01b548f33eb1937d15
maks0705 <41786473+maks0705@users.noreply.github.com>
2018-09-27 18:29:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "addition.h"
+
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 20; 
+    int c = addition(a,b);
+
+    cout << c << endl;
+
+    return 0;
+}
 
